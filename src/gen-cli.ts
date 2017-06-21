8dff539b788457efd198a9a046c9ebedd9c1d06d
吴学谦 <wuxueqian@ezbuy.com>
2017-06-21 13:43:39 +0800

    Make -w optional and add -v command

--- src/gen-cli.ts
+++ src/gen-cli.ts
@@ -6,9 +6,14 @@ import * as mkdirp from 'mkdirp';
 import * as yargs from 'yargs';
 
 const argv = yargs
+    .version(function() {
+        return require('../package.json').version;
+    })
+    .alias('v', 'version')
     .usage('Usage: $0 <command> [options]')
     .example('$0 -i ./member.thrift -o ./services -w ./webApi', '')
-    .demandOption(['i', 'o'])
+    .demandOption(['i'])
+    .default('o', './services')
     .default('w', './webApi')
     .describe('i', 'Input thrift file path')
     .describe('o', 'Ouput typescript file folder')
